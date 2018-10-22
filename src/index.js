2f2902d61f23cc999b1e2df6e8c77fd1da476851
Your Name <doiverson.canada@gmail.com>
2018-10-22 16:48:44 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './component/search_bar';
+
 
 const API_KEY = 'AIzaSyDNf1mNPWlrstR2mBpHiEYNjqY9hoO_CeQ'
 
 
 const App = () => {
-    return <div>Hi!</div>;
+    return(
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(
