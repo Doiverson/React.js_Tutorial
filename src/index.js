db575367f5534d962c1022ee9751846f3a5637f7
Your Name <doiverson.canada@gmail.com>
2018-10-22 16:22:59 -0700

    add all files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyDNf1mNPWlrstR2mBpHiEYNjqY9hoO_CeQ'
+
+
+const App = () => {
+    return <div>Hi!</div>;
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('.container')
+);
